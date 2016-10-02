e65ac6f03079f968adee45b8ff6237cc87c25ccb
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2016-10-01 20:29:54 -0500

    Update _001TwoSum.java

--- LeetCodePractice/_001TwoSum.java
+++ LeetCodePractice/_001TwoSum.java
@@ -1,7 +1,6 @@
 package LeetCodePractice;
 
 import java.util.Arrays;
-
 public class _001TwoSum {
 	public static void main(String[] args) {
 		System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
